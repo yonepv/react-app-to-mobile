1cd3a8445300832d14c8ec60b9e4b808d4435a4a
Rocio Philipps <rocio.philippsvalencia@mcn.gouv.qc.ca>
2024-05-06 16:55:10 -0400

    removed dependency composantsweb
    Signed-off-by: Rocio Philipps <rocio.philippsvalencia@mcn.gouv.qc.ca>

--- src/components/testIncludeComposantsWeb/testIncludeComposantsWeb.tsx
+++ src/components/testIncludeComposantsWeb/testIncludeComposantsWeb.tsx
@@ -1,12 +1,10 @@
 import React, { FC } from 'react';
-//import Bouton from 'poc/src/sqin-ux/src/Composants/Bouton/Bouton';
 
 interface TestIncludeComposantsWebProps {}
 
 const TestIncludeComposantsWeb: FC<TestIncludeComposantsWebProps> = () => (
   <div>
     TestIncludeComposantsWeb Component
-    {/* <Bouton /> */}
     
 
   </div>
